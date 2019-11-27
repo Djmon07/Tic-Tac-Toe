5140cd43f1f3d3e4ee9920ecec18c4bdf03a2a9f
Cody Martin <codemart10@gmail.com>
2019-11-27 11:07:07 -0500

    touch ups to js and css

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//grid
 const cells = {
     one: document.body.querySelector("#one"),
     two: document.body.querySelector("#two"),
@@ -10,88 +11,100 @@ const cells = {
     nine: document.body.querySelector("#nine"),
 };
 
+//reset button
 document.body.addEventListener("click", event => {
   if (event.target.nodeName == "BUTTON") {
     window.location.reload(true);
   }
 });
 
+//win tracker
 let result = 0;
 
 //Random turn
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
+
+//turn tracker
 let turn = (getRandomInt(2));
 
+//Tie tracker
 let gamecounter = 0;
 
+//turns
 document.body.addEventListener("click", click = event => {
 
-
-//turns
   if (event.target.nodeName == "DIV"){
 
     if (event.target.textContent != "X" && event.target.textContent != "O"){
-        turn = turn + 1;
+        turn++;
         gamecounter++;
     }
-
-    if (turn == 0 || turn % 2 == 0 && (event.target.textContent != "O" && event.target.textContent != "X")){
+    if (turn == 0 || turn % 2 == 0 &&
+      (event.target.textContent != "O" && event.target.textContent != "X")){
         event.target.textContent = "X";
 
     }
-
-    if (turn == 1 || turn % 2 != 0 && (event.target.textContent != "X" && event.target.textContent != "O")){
+    if (turn == 1 || turn % 2 != 0 &&
+      (event.target.textContent != "X" && event.target.textContent != "O")){
         event.target.textContent = "O";
     }
-
   }
 
-//win condition
-    if (cells.one.textContent == "X"){
-      if ((cells.seven.textContent == "X" && cells.four.textContent == "X") || (cells.three.textContent == "X" && cells.two.textContent == "X")){
-        result++;
+//win conditions
 
-      }
+//If X Wins
+  if (cells.one.textContent == "X"){
+    if ((cells.seven.textContent == "X" && cells.four.textContent == "X") ||
+        (cells.three.textContent == "X" && cells.two.textContent == "X")){
+          result++;
+
     }
-
-    if (cells.nine.textContent == "X"){
-      if ((cells.three.textContent == "X" && cells.six.textContent == "X") || (cells.seven.textContent == "X" && cells.eight.textContent == "X")){
-        result++;
-      }
+  }
+  if (cells.nine.textContent == "X"){
+    if ((cells.three.textContent == "X" && cells.six.textContent == "X") ||
+        (cells.seven.textContent == "X" && cells.eight.textContent == "X")){
+          result++;
     }
-
+  }
   if (cells.five.textContent == "X"){
 
-    if (((cells.one.textContent == "X" && cells.nine.textContent == "X") || (cells.three.textContent == "X" && cells.seven.textContent == "X")) ||
-        ((cells.two.textContent == "X" && cells.eight.textContent == "X") || (cells.four.textContent == "X" && cells.six.textContent == "X"))) {
-        result++;
+    if (((cells.one.textContent == "X" && cells.nine.textContent == "X") ||
+         (cells.three.textContent == "X" && cells.seven.textContent == "X"))
+         ||
+        ((cells.two.textContent == "X" && cells.eight.textContent == "X") ||
+         (cells.four.textContent == "X" && cells.six.textContent == "X"))) {
+           result++;
     }
   }
 
+//If O Wins
   if (cells.one.textContent == "O"){
-    if ((cells.seven.textContent == "O" && cells.four.textContent == "O") || (cells.three.textContent == "O" && cells.two.textContent == "O")){
-      result--;
+    if ((cells.seven.textContent == "O" && cells.four.textContent == "O") ||
+        (cells.three.textContent == "O" && cells.two.textContent == "O")){
+          result--;
       }
     }
-
   if (cells.nine.textContent == "O"){
 
-    if ((cells.three.textContent == "O" && cells.six.textContent == "O") || (cells.seven.textContent == "O" && cells.eight.textContent == "O")){
-      result--;
+    if ((cells.three.textContent == "O" && cells.six.textContent == "O") ||
+        (cells.seven.textContent == "O" && cells.eight.textContent == "O")){
+          result--;
       }
     }
-
   if (cells.five.textContent == "O"){
 
-    if (((cells.one.textContent == "O" && cells.nine.textContent == "O") || (cells.three.textContent == "O" && cells.seven.textContent == "O")) ||
-       ((cells.two.textContent == "O" && cells.eight.textContent == "O") || (cells.four.textContent == "O" && cells.six.textContent == "O"))) {
-      result--;
+    if (((cells.one.textContent == "O" && cells.nine.textContent == "O") ||
+         (cells.three.textContent == "O" && cells.seven.textContent == "O"))
+         ||
+        ((cells.two.textContent == "O" && cells.eight.textContent == "O") ||
+         (cells.four.textContent == "O" && cells.six.textContent == "O"))) {
+           result--;
     }
   }
 
-  //Winner
+  //Winner Display
   if (gamecounter == 9){
     let tie = document.body.querySelector("#won");
     tie.textContent = "Tie!";
